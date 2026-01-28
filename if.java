8be82b811d972e066debb93b68abd212cb254902
0xEiCore <146795260+oxsh0gunX@users.noreply.github.com>
2026-01-29 02:09:16 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,17 @@
+class Op {
+    public static void main(String[] args){
+        int num_1=3;
+        int num_2=4;
+        int num_3=2;
+        // System.out.println("num1 + num2= " + (num_1 + num_2));
+        // System.out.println("num1-num2 = "+ (num_1 - num_2));
+        // System.out.println("num1*num2 = "+ (num_1 *num_2));
+        // System.out.println("num1/num2 = "+ (num_1 /num_2));
+        if( num_1 > num_2){
+            System.out.println("num_1 is greater");
+        }else{
+            System.out.println("num_2 is greater");
+        }
+        
+    }
+}
