78fffb39bc7874dca79747a0ce3a00b48155d2ed
0xEiCore <146795260+oxsh0gunX@users.noreply.github.com>
2026-01-29 00:29:51 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,11 @@
+class variable{
+    
+    public static void main(String[] args){
+        int age=34;
+        double num=34.2;
+        char character='a';
+        System.out.println("The age is "+age);
+        System.out.println("The number is "+num);
+        System.out.println("The character is "+character);
+    }
+}
