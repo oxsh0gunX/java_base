d901bd4ca11bc46c24d83eca9e6c465b8f05fb37
0xEiCore <146795260+oxsh0gunX@users.noreply.github.com>
2026-01-29 01:43:03 +0530

    Create arops.java

---
+++ arops.java
@@ -0,0 +1,12 @@
+class Op {
+    public static void main(String[] args){
+        int num_1=3;
+        int num_2=4;
+        int num_3=2;
+        System.out.println("num1 + num2= " + (num_1 + num_2));
+        System.out.println("num1-num2 = "+ (num_1 - num_2));
+        System.out.println("num1*num2 = "+ (num_1 *num_2));
+        System.out.println("num1/num2 = "+ (num_1 /num_2));
+        
+    }
+}
