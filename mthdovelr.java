eb1ffa3bad6611c21fd7623db4f0a94e3eb498e9
paititi urko <146795260+oxsh0gunX@users.noreply.github.com>
2026-04-05 15:52:56 +0530

    Add method overloading for find_sum in Main class
    Implement method overloading for find_sum with two and three parameters.

---
+++ mthdovelr.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+         find_sum(3,4);
+        find_sum(3,4,5);
+    }
+    public static void  find_sum(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void  find_sum(int a , int b ,int c){
+        System.out.println(a+b+c);
+    }
+    }
+    
+    // mehord overloading same name difren parameer
