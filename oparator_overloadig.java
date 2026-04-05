27fe271527bd133d80dc1a3901250c8bc9357fbd
paititi urko <146795260+oxsh0gunX@users.noreply.github.com>
2026-04-06 01:47:28 +0530

    Implement basic operator overloading example
    Demonstrate basic operator overloading with integers and strings.

---
+++ oparator_overloadig.java
@@ -0,0 +1,13 @@
+class Main {
+
+
+public static void main (String args[]){
+int num1 =5;
+int num2 =6;
+System.out.println(num1+num2);
+String fname="ei";
+String lname="sama";
+System.out.println(fname+lname);
+
+}
+}
